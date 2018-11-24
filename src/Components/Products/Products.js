fd00a868988ab6eb29b0795a34f22924b64d7730
Andy Million <amillion3@gmail.com>
2018-11-23 21:45:42 -0600

    Having CORS issues on the backend

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -4,7 +4,8 @@ import React, {Component} from 'react';
 class Products extends Component {
   state = {
     products: [],
-    productId: 0
+    productId: 0,
+    productDeleteId: 0
   };
 
   clickGetAllProducts = () => {
@@ -18,26 +19,49 @@ class Products extends Component {
     })
   }
 
+  // Single Product
   clickGetSingleProduct = () => {
     const url = `https://localhost:44398/api/product/${this.state.productId}`;
-    fetch(url)
+    return fetch(url)
     .then(product => product.json())
     .then(products => {
+      console.log(products);
       this.setState({
         products
       })
     })
   }
-
   handleSingleProduct = e => {
     const {name, value} = e.target;
     this.setState({
       [name]:value
     });
   }
-
+  // Delete Product
+  clickDeleteProduct = () => {
+    const url = `https://localhost:44398/api/product/${this.state.productDeleteId}`;
+    console.log(`https://localhost:44398/api/product/${this.state.productDeleteId}`);
+    return fetch(url, {
+      method: 'delete'
+    })
+    .then(product => product.json())
+    .then(products => {
+      console.log(products);
+      this.setState({
+        products
+      });
+    })
+  }
+  handleDeleteProduct = e => {
+    const {name, value} = e.target;
+    console.log(e.target.value);
+    console.log(e.target.name);
+    this.setState({
+      [name]: value
+    });
+  }
   render () {
-    const {products, productId} = this.state;
+    const {products, productId, productDeleteId} = this.state;
 
     const output = products.map((product) => {
       return (
@@ -70,6 +94,22 @@ class Products extends Component {
               value="Get Single Product"
               onClick={this.clickGetSingleProduct}
               />
+          </form>
+        </div>
+        <div>
+          <form>
+            <label>Delete Product By Id:</label>
+            <input
+              type="number"
+              name="productDeleteId"
+              value={productDeleteId}
+              onChange={this.handleDeleteProduct}
+            />
+            <input
+              type="button"
+              value="Delete"
+              onClick={this.clickDeleteProduct}
+            />
           </form>
         </div>
         <ul>{output}</ul>
